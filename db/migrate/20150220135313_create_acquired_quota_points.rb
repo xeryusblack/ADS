2faaba67d42c46e9c414c7b3c7da50c99a975b83
Wilbert <wilbertxeryus@gmail.com>
2015-02-25 11:51:24 +0800

    Added Dump File named ads_backup in config folder

--- db/migrate/20150220135313_create_acquired_quota_points.rb
+++ db/migrate/20150220135313_create_acquired_quota_points.rb
@@ -2,7 +2,7 @@ class CreateAcquiredQuotaPoints < ActiveRecord::Migration
   def change
     create_table :acquired_quota_points do |t|
     	t.decimal :amount, null: false, :default => 0.00
-    	t.date :date_conducted, null: false, :default => DateTime.now
+    	t.date :date_conducted, null: false, :default => Date.today
     	t.belongs_to :varsity_member, null: false, index: true
     	t.belongs_to :training_activity, null: false, index: true
       # t.timestamps
