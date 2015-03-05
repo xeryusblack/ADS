6ca4b25821c0564b36b24d7aa10b17be71e25e15
Wilbert <wilbertxeryus@gmail.com>
2015-03-05 17:43:50 +0800

    added devise views

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,6 @@ Rails.application.routes.draw do
   resources :contingents
   resources :acquired_quota_points
 
-  
-
   devise_for :officer_in_charges
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
