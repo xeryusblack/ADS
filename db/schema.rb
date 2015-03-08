f13b772aa398f2c6bdba46fe7bd3bc4e384bd590
Wilbert <wilbertxeryus@gmail.com>
2015-03-08 12:43:33 +0800

    added config.ru

--- db/schema.rb
+++ db/schema.rb
@@ -105,24 +105,6 @@ ActiveRecord::Schema.define(version: 20150307101346) do
     t.string "debater_position",                        null: false
   end
 
-  create_table "users", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-
   create_table "varsity_members", force: true do |t|
     t.string  "first_name",                                                                      null: false
     t.string  "last_name",                                                                       null: false
