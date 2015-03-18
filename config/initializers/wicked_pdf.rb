55b143b8d5464f46d45a6308eda80fa2bbfb5698
March826 <chua_mari5_ksst@yahoo.com>
2015-03-18 21:45:00 +0800

    working pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,5 +2,6 @@ WickedPdf.config = {
   #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
   #:layout => "pdf.html",
   #:exe_path => '/usr/local/bin/wkhtmltopdf'
-  :exe_path => '#{Rails.root}/bin/wkhtmltopdf'
+  #:exe_path=>"#{RAILS_ROOT}/pdfbin/wkhtmltopdf.exe"
+  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf'
 }
