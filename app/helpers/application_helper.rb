d74a7d7d1d11196909d27a1ec4f034c30f9602bb
March826 <chua_mari5_ksst@yahoo.com>
2015-03-22 22:17:03 +0800

    create user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+	def resource_class 
+		OfficerInCharge
+	end
+	def resource_name
+    :officer_in_charge
+  end
+
+  def resource
+    @resource ||= OfficerInCharge.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:officer_in_charge]
+  end
 end
